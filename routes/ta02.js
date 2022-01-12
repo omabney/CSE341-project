3d837300823f5f80a61244959376c39a27c6677f
omabney <om.gopalrao@gmail.com>
2022-01-12 08:12:38 -0700

    Finished with team activity 02

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,15 +1,42 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
+// TA02 Solution
+// This is the solution for the routing side of TA02
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('pages', {
-    title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
+//Team activity 01
+const userArray = ['Tony', 'Bruce', 'Peter'];
+
+//Team activity 02
+router.post('/addUser', (req, res, next) => {
+    const newUser = req.body.newUser;
+
+    userArray.push(newUser);
+
+    res.redirect('/ta02/');
+});
+
+//Team activity 03
+router.post('/removeUser', (req, res, next) => {
+    const remUser = req.body.remUser;
+
+    
+    const index = userArray.indexOf(remUser);
+    if (index !== -1 ) {
+        userArray.splice(index, 1);
+    }
+
+    res.redirect('/ta02/');
+});
+
+router.get('/',(req, res, next) => {
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        users: userArray,
+        path: '/ta02', // For pug, EJS
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
 });
 
 module.exports = router;
+

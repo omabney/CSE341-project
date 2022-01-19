4d0afa05d049da8dc515c846ce9ba0f754052bac
omabney <om.gopalrao@gmail.com>
2022-01-18 20:47:55 -0700

    Completed Team Activity 03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,9 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const ta03Controller = require('../controllers/ta03.js');
 
-router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
-});
+router.get('/', ta03Controller.getProducts);
+router.get('/search', ta03Controller.getSearchProducts);
 
 module.exports = router;
